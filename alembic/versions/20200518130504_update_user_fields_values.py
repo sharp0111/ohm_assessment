359d9fc49a7e5a9d2a6d4968fa4451a39bb58628
Res Yeg <shaqk702@gmail.com>
2020-05-18 14:02:14 -0700

    Create migration for task 2
    Increase the point_balance for user 1 to 5000
    Add a location for user 2, assuming they live in the USA
    Change the tier for user 3 to Silver

---
+++ alembic/versions/20200518130504_update_user_fields_values.py
@@ -0,0 +1,36 @@
+"""update user fields values
+
+Revision ID: ab7fb1ae4bb
+Revises: 00000000
+Create Date: 2020-05-18 13:05:04.615771
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = 'ab7fb1ae4bb'
+down_revision = '00000000'
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    op.execute('''UPDATE user
+                    SET point_balance=0
+                    WHERE user_id=1
+    ''')
+
+    op.execute('''INSERT INTO rel_user (user_id, rel_lookup, attribute)
+                  VALUES
+                        (2, 'LOCATION', 'USA')
+    ''')
+
+    op.execute('''UPDATE user
+                        SET tier='Silver'
+                        WHERE user_id=3
+    ''')
+
+
+def downgrade():
+    op.execute("TRUNCATE TABLE user")
+    op.execute("TRUNCATE TABLE rel_user")
