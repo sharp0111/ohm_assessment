c88896a677efb13c9b505ea8d684b3f3658d5af6
Res Yeg <shaqk702@gmail.com>
2020-05-19 14:40:58 -0700

    Add user tests

--- tests/models_tests/user_test.py
+++ tests/models_tests/user_test.py
@@ -3,5 +3,32 @@ from tests import OhmTestCase
 
 class UserTest(OhmTestCase):
     def test_get_multi(self):
-        assert self.chuck.get_multi("PHONE") == ['+14086441234', '+14086445678']
+        assert self.chuck.get_multi("PHONE") == [
+            '+14086441234', '+14086445678']
         assert self.justin.get_multi("PHONE") == []
+
+    def test_full_name(self):
+        assert self.chuck.full_name() == "Chuck Norris"
+        assert self.elvis.full_name() == "Elvis Presley"
+        assert self.justin.full_name() == "Justin Bieber"
+
+    def test_get_tier(self):
+        assert self.chuck.get_tier() == "Carbon"
+        assert self.elvis.get_tier() == "Carbon"
+        assert self.justin.get_tier() == "Silver"
+
+    def test_get_points_and_dollars(self):
+        assert self.chuck.get_points_and_dollars()["points"] == 5000
+
+    def test_is_below_tier(self):
+        assert self.chuck.is_below_tier("Platinum") == True
+        assert self.chuck.is_below_tier("Gold") == True
+        assert self.chuck.is_below_tier("Silver") == True
+        assert self.chuck.is_below_tier("Bronze") == True
+        assert self.chuck.is_below_tier("Carbon") == False
+
+        assert self.justin.is_below_tier("Platinum") == True
+        assert self.justin.is_below_tier("Gold") == True
+        assert self.justin.is_below_tier("Silver") == False
+        assert self.justin.is_below_tier("Bronze") == False
+        assert self.justin.is_below_tier("Carbon") == False
