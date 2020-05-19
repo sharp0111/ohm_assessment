34c7b2fbbaa51a19edd06444f661ce481147e5b7
Res Yeg <shaqk702@gmail.com>
2020-05-19 11:15:13 -0700

    Implement community method: query users from db

--- pages/community.py
+++ pages/community.py
@@ -3,22 +3,22 @@ from flask.ext.login import current_user
 from sqlalchemy import desc
 
 from functions import app
-from models import User
+from models import User, RelUserMulti
 
 
 @app.route('/community', methods=['GET'])
 def community():
 
-  if current_user.is_authenticated():
-    users = User.query.order_by(desc(User.signup_date)).limit(5).all()
+    if current_user.is_authenticated():
+        users = User.query.order_by(desc(User.signup_date)).limit(5).all()
 
-    args = {
-        'gift_card_eligible': True,
-        'cashout_ok': True,
-        'user_below_silver': current_user.is_below_tier('Silver'),
-        'users': users
-    }
+        args = {
+            'gift_card_eligible': True,
+            'cashout_ok': True,
+            'user_below_silver': current_user.is_below_tier('Silver'),
+            'users': users
+        }
 
-    return render_template("community.html", **args)
-  else:
-    return redirect("/")
+        return render_template("community.html", **args)
+    else:
+        return redirect("/")
