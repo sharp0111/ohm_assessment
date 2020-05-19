df3996374e341cd8c3e6bf6999d879c2a222a6d9
Res Yeg <shaqk702@gmail.com>
2020-05-19 14:45:18 -0700

    Login user 1 to match with dashboard page

--- pages/community.py
+++ pages/community.py
@@ -1,5 +1,5 @@
 from flask import jsonify, render_template, request, Response, redirect
-from flask.ext.login import current_user
+from flask.ext.login import current_user, login_user
 from sqlalchemy import desc
 
 from functions import app
@@ -9,16 +9,15 @@ from models import User, RelUserMulti
 @app.route('/community', methods=['GET'])
 def community():
 
-    if current_user.is_authenticated():
-        users = User.query.order_by(desc(User.signup_date)).limit(5).all()
+    login_user(User.query.get(1))
 
-        args = {
-            'gift_card_eligible': True,
-            'cashout_ok': True,
-            'user_below_silver': current_user.is_below_tier('Silver'),
-            'users': users
-        }
+    users = User.query.order_by(desc(User.signup_date)).limit(5).all()
 
-        return render_template("community.html", **args)
-    else:
-        return redirect("/")
+    args = {
+        'gift_card_eligible': True,
+        'cashout_ok': True,
+        'user_below_silver': current_user.is_below_tier('Silver'),
+        'users': users
+    }
+
+    return render_template("community.html", **args)
