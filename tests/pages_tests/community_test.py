f07fcb4f8b8ec3e9124b0c1f89f6fce5be0cb2d0
Res Yeg <shaqk702@gmail.com>
2020-05-19 14:44:55 -0700

    Add test for community page

---
+++ tests/pages_tests/community_test.py
@@ -0,0 +1,12 @@
+
+from app_main import app
+from tests import OhmTestCase
+
+
+class CommunityTest(OhmTestCase):
+    def test_get(self):
+        with app.test_client() as c:
+            response = c.get('/community')
+            assert "Chuck Norris" in response.data
+            assert "Elvis Presley" in response.data
+            assert "Justin Bieber" in response.data
